54c8380427a7467f471d8dbe73c0751c9762939b
AzureAD\AbbyYelton <ayelton@composablesystems.net>
2017-01-10 10:33:56 -0500

    Initial commit

---
+++ Controllers/Web/AppController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MGC.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult AddGift()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/GiftViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGC.ViewModels
+{
+    public class GiftViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        [StringLength(200)]
+        public string StoreLink { get; set; }
+
+        //   public Recipient Recipient { get; set; }
+        //   public Holiday Holiday { get; set; }
+        public string Notes { get; set; }
+        public bool Purchased { get; set; }
+        public bool Wrapped { get; set; }
+    }
+}
